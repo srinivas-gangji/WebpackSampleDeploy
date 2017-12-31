c4b9e5fc5c007e88da26c5bc329e956b9d8bcca4
sgangj200 <srinivasrao_gangji@comcast.com>
2017-12-31 00:34:01 -0700

    Vendor code splitting and managing script tag injection

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,28 @@
 var webpack = require("webpack")
 var path = require("path")
+var HtmlWebpackPlugin = require("html-webpack-plugin")
+
+//List of vendor modules
+const VENDOR_LIBS = [
+  "react",
+  "lodash",
+  "redux",
+  "react-redux",
+  "react-dom",
+  "faker",
+  "react-input-range",
+  "redux-form",
+  "redux-thunk"
+]
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    bundle: "./src/index.js",
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, "dist"),
-    filename: "bundle.js"
+    filename: "[name].js"
   },
   module: {
     rules: [
@@ -19,5 +36,13 @@ module.exports = {
         test: /\.css$/
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "vendor"
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
 }
